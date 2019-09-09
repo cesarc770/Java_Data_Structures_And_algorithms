62d2a27292f265dbea0663c13f6f84e7bfa2f805
Cesar Caceres <cesarc770@gmail.com>
2019-09-09 14:09:31 -0600

    updated class to be circular and added extra excercise using circularity

--- CircularSingleLinkedList/src/CircularSingleLinkedList.java
+++ CircularSingleLinkedList/src/CircularSingleLinkedList.java
@@ -9,6 +9,7 @@ public class CircularSingleLinkedList<T> {
     //creation
     public CircularSingleLinkedList(T nodeValue) {
         CircularSingleNode<T> node = new CircularSingleNode<>(nodeValue);
+        node.setNext(node);
         this.head = node;
         this.tail = node;
         this.size = 1;
@@ -50,9 +51,11 @@ public class CircularSingleLinkedList<T> {
         } else if (location == 0) {
             newNode.setNext(this.head);
             this.head = newNode;
+            this.tail.setNext(newNode);
         } else if (location >= size) {
             this.tail.setNext(newNode);
             this.tail = newNode;
+            this.tail.setNext(this.head);
         } else {
             CircularSingleNode<T> tempNode = head;
             int index = 0;
@@ -71,7 +74,7 @@ public class CircularSingleLinkedList<T> {
     }
 
     //traversing
-    public void traverseSLL() {
+    public void traverseCSLL() {
         if (this.existsLinkedList(this.getHead())) {
             CircularSingleNode<T> tempNode = this.getHead();
             for(int i = 0; i < this.getSize(); i++){
@@ -96,6 +99,7 @@ public class CircularSingleLinkedList<T> {
             return;
         } else if (location == 0) {
             this.head = this.head.getNext();
+            this.tail.setNext(this.head);
             setSize(getSize()-1);
             if(getSize() == 0) {
                 tail = null;
@@ -112,7 +116,7 @@ public class CircularSingleLinkedList<T> {
                 return;
             }
 
-            tempNode.setNext(null);
+            tempNode.setNext(this.head);
             this.tail = tempNode;
             this.setSize(this.getSize()-1);
 
@@ -129,8 +133,28 @@ public class CircularSingleLinkedList<T> {
 
     //deletion of linkedlist
     public void deleteLinkedList() {
-        this.head = this.tail = null;
+        this.head = null;
+        tail.setNext(null);
+        tail = null;
         System.out.println("Deleted Linked List");
+    }
+
+    //Search in Linked List
+    boolean search(T nodeValue) {
+        if (existsLinkedList(this.head)) {
+            CircularSingleNode<T> tempNode = head;
+            for (int i = 0; i < size; i++) {
+
+                if (tempNode.getValue() == nodeValue) {
+                    System.out.print("Found the node at location: "+i);
+                    return true;
+                }
+                tempNode = tempNode.getNext();
+            }
+        }
+        System.out.print("Node not found!! ");
+
+        return false;
     }
 
 
@@ -140,8 +164,33 @@ public class CircularSingleLinkedList<T> {
         return head != null;
     }
 
+///EXTRA////
+    //I want to print the list as many times as the user wants using the get next method
+    // and since it is circular it will continue to loop until terminated somehow else.
+    //in this case I am using times and keeping the round variable to terminate the loop
+    public void traverseWithCircularProperties (int times) {
+        int count = 0;
+        int round = 0;
+        CircularSingleNode<T> tempNode = this.getHead();
+            if (this.existsLinkedList(this.getHead())) {
+                while(tempNode.getNext() != null && round < times ){
+                    System.out.print(tempNode.getValue());
+                    count++;
+                    if(count < this.getSize()) {
+                        System.out.print(" -> ");
+                    } else {
+                        count = 0;
+                        round++;
+                        System.out.println("");
+                    }
+                    tempNode = tempNode.getNext();
+                }
+            }else {
+                System.out.println("Linked List does not exists !");
+            }
+            System.out.println("\n");
 
-
+    }
 
 
 }
--- CircularSingleLinkedList/src/CircularSingleLinkedListMain.java
+++ CircularSingleLinkedList/src/CircularSingleLinkedListMain.java
@@ -1,6 +1,66 @@
 public class CircularSingleLinkedListMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Single Linked List");
+
+        //generate new Linked List
+        CircularSingleLinkedList<Integer> myLinkedList = new CircularSingleLinkedList<>(10);
+
+        System.out.println("Value of recently created LinkedList head " + myLinkedList.getHead().getValue());
+        System.out.println("Value of recently created LinkedList tail " + myLinkedList.getHead().getValue());
+
+        //tail and head should be the same since it only has one node
+        System.out.println(myLinkedList.getHead() == myLinkedList.getHead());
+        System.out.println("Traversing List values:");
+        myLinkedList.traverseCSLL();
+
+        //Inserting values
+        System.out.println("\nInserting into list at the end:");
+        myLinkedList.insertInLinkedList(20, myLinkedList.getSize());
+        myLinkedList.traverseCSLL();
+
+        System.out.println("\nInserting into list at the end:");
+        myLinkedList.insertInLinkedList(30, myLinkedList.getSize());
+        myLinkedList.traverseCSLL();
+
+        System.out.println("\nInserting into list at the beginning:");
+        myLinkedList.insertInLinkedList(5, 0);
+        myLinkedList.traverseCSLL();
+
+        System.out.println("\nInserting into list in the middle:");
+        myLinkedList.insertInLinkedList(15, 2);
+        myLinkedList.traverseCSLL();
+
+        System.out.println("\nInserting into list in the middle:");
+        myLinkedList.insertInLinkedList(25, 4);
+        myLinkedList.traverseCSLL();
+
+        //EXTRA: testing circularity of linked list
+        System.out.println("Traversing 5 times");
+        myLinkedList.traverseWithCircularProperties(5);
+        System.out.println("Traversing 3 times");
+        myLinkedList.traverseWithCircularProperties(3);
+
+
+        //deleting from Linked List
+        System.out.println("\nDeleting from list at the beginning:");
+        myLinkedList.deleteFromLinkedList(0);
+        myLinkedList.traverseCSLL();
+
+        System.out.println("\nDeleting from list in the middle:");
+        myLinkedList.deleteFromLinkedList(2);
+        myLinkedList.traverseCSLL();
+
+        System.out.println("\nDeleting from list at the end:");
+        myLinkedList.deleteFromLinkedList(5);
+        myLinkedList.traverseCSLL();
+
+        //Delete whole Linked List
+        System.out.println("\nDeleting complete Linked List:");
+        myLinkedList.deleteLinkedList();
+        myLinkedList.traverseCSLL();
+
+
+
     }
 }
